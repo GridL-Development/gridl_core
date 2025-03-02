a35286e17cd73eb5e20f961fe891b4fd464bfe9d
joselvelez <joselvelez@protonmail.com>
2025-03-02 16:10:09 -0300

    update constants, add attributes constants

---
+++ src/constants/attributes.ts
@@ -0,0 +1,40 @@
+/** @format */
+
+export const requiredColumnsForAccountAttributeImport = [
+  "name",
+  "code",
+  "description",
+];
+
+export const requiredColumnsForAccountAttributeValueImport = [
+  "name",
+  "code",
+  "parent",
+  "attribute_code",
+];
+
+export const requiredColumnsForLevelAttributeImport = [
+  "name",
+  "code",
+  "description",
+];
+
+export const requiredColumnsForLevelAttributeValueImport = [
+  "name",
+  "code",
+  "parent",
+  "attribute_code",
+];
+
+export const requiredColumnsForDimensionValueAttributeImport = [
+  "name",
+  "code",
+  "description",
+];
+
+export const requiredColumnsForDimensionValueAttributeValueImport = [
+  "name",
+  "code",
+  "attribute_code",
+  "parent",
+];
