d897bdfa654a9ae6f3f47997fd34f0e88f742611
joselvelez <joselvelez@protonmail.com>
2025-03-19 09:05:33 -0300

    update CSVDimensionImportContext

--- src/types/commonTypes.ts
+++ src/types/commonTypes.ts
@@ -97,7 +97,7 @@ export interface CSVDimensionImportContext {
   instanceId: string;
   existingCodes: Set<string>;
   existingNames: Set<string>;
-  existingRecords: Map<string, { code: string; name: string }>;
+  existingRecords?: Map<string, { code: string; name: string }>;
 }
 
 export interface CSVDimensionValueImportContext {
